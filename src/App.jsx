9e15600c0ef33539e615f721a9569b669085651b
elisimo-137 <ela.r.macek@gmail.com>
2024-05-22 18:00:17 +0200

    zakljucna

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,21 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function App() {
   const [data, setData] = useState([]);
   const [obcine, setObcine] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectedObcina, setSelectedObcina] = useState("all");
 
   async function getSchools() {
     const response = await fetch("http://static.404.si/grace/");
@@ -35,28 +46,45 @@ export default function App() {
       <div className="container mb-4 mt-4">
         <div className="flex gap-4">
           {/* Ne pozabi na onValueChange event. */}
-          <Select>
+          <Select onValueChange={(value) => setSelectedObcina(value)}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Regija" />
             </SelectTrigger>
             <SelectContent>
               <SelectItem value="all">Vse občine</SelectItem>
+              {obcine.map((obcina) => (
+                <SelectItem value={obcina}>{obcina}</SelectItem>
+              ))}
               {/* Uporabi map funkcijo, ki se bo sprehodila, čez vse občine in jih prikazala v obliki SelectItemov. */}
             </SelectContent>
           </Select>
-
+          <Input
+            placeholder="Iskanje po poštni številki..."
+            type="number"
+            onChange={(value) => setSearch(value.currentTarget.value)}
+          ></Input>
           {/* Dodaj input, ki bo omogčal iskanje po poštni številki. Ne pozabi na onChange event. */}
         </div>
       </div>
       <div className="container">
-        <div class="">
+        <div className="grid grid-cols-3 gap-4">
           {/* Uporabi map funkcijo, ki se bo sprehodila, čez vse šole in jih prikazala v obliki kartic. */}
           {/* Dodaj dva filtra: enega za filtriranje po obcini, drugega za filtriranje glede na poštno številko šole. */}
-          {data.map((school) => (
-            <div>
-              <School data={school}></School>
-            </div>
-          ))}
+          {data
+            .filter(
+              (school) =>
+                school.obcina == selectedObcina || selectedObcina == "all",
+            )
+            .filter(
+              (school) =>
+                school.postna_stevilka.toString().startsWith(search) ||
+                search == "",
+            )
+            .map((school) => (
+              <div>
+                <School data={school}></School>
+              </div>
+            ))}
         </div>
       </div>
     </>
--- src/School.jsx
+++ src/School.jsx
@@ -1,5 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function School(props) {
   const { regija, obcina, naziv, naslov, postna_stevilka, posta, email } =
     props.data;
-  return <p>Občina</p>;
+  return (
+    <Card className="">
+      <CardHeader>
+        <CardTitle>{naziv}</CardTitle>
+        <CardDescription>{regija}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          <span className="font-bold">Naslov:</span> {naslov}, poštna številka:{" "}
+          {postna_stevilka}, {posta}
+        </p>
+      </CardContent>
+      <CardFooter>
+        <p>
+          <strong>E- poštni naslov: </strong>{" "}
+          <a href={`mailto:${email}`}> {email}</a>
+        </p>
+      </CardFooter>
+    </Card>
+  );
 }
